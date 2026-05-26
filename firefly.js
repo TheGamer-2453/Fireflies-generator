e6fd84b8a573550a41688ae31bd4307cf0be983c
TheGamer <85999301+TheGamer-2453@users.noreply.github.com>
2026-05-26 23:10:38 +0530

    made fireflies

---
+++ firefly.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('fireflyCanvas');
+const ctx = canvas.getContext('2d')
+
+function resizeCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.inertHeight;
+}
+
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+
+const firefiles = [];
+
+class firefiy {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        
+        this.radius = Math.random() * 2.5 + 1.5;
+
+        this.vx = (Math.random() - 0.5) * 3;
+        this.vy = (Math.random() - 0.6) * 2 - 0.5;
+
+        this.alpha = 1
+        this.decay = Math.random()* 0.01 + 0.005
+        this.angle = Math.random()* Math.Pi* 2
+        this.speedMultiplier = Math.random()* 0.05 + 0.02;
+    }
+update() {
+    this.angle += this.speedMultiplier;
+    this.x += this.vx + Math.sin(this.angle) * 0.5;
+    this.y += this.vy;     
+    this.alpha -= this.decay;
+}
+}
